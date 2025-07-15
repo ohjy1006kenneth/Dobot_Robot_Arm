6d43cffbd4d3000f27f86ac5c85a356aee87273f
ohjy1006kenneth <ohjuyoung1006@gmail.com>
2025-07-14 22:05:27 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install ros-humble-joint-state-broadcaster
+sudo apt install ros-humble-ros2controlcli
+sudo apt install ros-humble-ros2-controllers
+sudo apt install ros-humble-moveit
+
---
+++ setup.sh
@@ -0,0 +1,6 @@
+source /opt/ros/humble/setup.sh
+source install/local_setup.sh
+
+export IP_address=192.168.201.1
+export DOBOT_TYPE=cr20
+
