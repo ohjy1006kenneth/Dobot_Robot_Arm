170663c292b66ada12d52611cf881c94f1a6a91a
ohjy1006kenneth <ohjuyoung1006@gmail.com>
2025-07-17 15:34:30 -0500

    added sdk

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 sudo apt update
+sudo apt-get update
 sudo apt install ros-humble-joint-state-broadcaster
 sudo apt install ros-humble-ros2controlcli
 sudo apt install ros-humble-ros2-controllers
 sudo apt install ros-humble-moveit
 
+# ugv_sdk
+sudo apt-get install build-essential git cmake libasio-dev
+
+# Femto Mega
+sudo apt install libgflags-dev nlohmann-json3-dev  \
+ros-$ROS_DISTRO-image-transport ros-${ROS_DISTRO}-image-transport-plugins ros-${ROS_DISTRO}-compressed-image-transport \
+ros-$ROS_DISTRO-image-publisher ros-$ROS_DISTRO-camera-info-manager \
+ros-$ROS_DISTRO-diagnostic-updater ros-$ROS_DISTRO-diagnostic-msgs ros-$ROS_DISTRO-statistics-msgs \
+ros-$ROS_DISTRO-backward-ros libdw-dev
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 source /opt/ros/humble/setup.sh
 source install/local_setup.sh
 
+eval "$(register-python-argcomplete3 ros2)"
+eval "$(register-python-argcomplete3 colcon)"
+
 export IP_address=192.168.201.1
 export DOBOT_TYPE=cr20
  
